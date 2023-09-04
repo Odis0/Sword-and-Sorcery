6b97ac0787ec17156ebfffb9cf82cbd4c100e249
Spencer Osburn <sjosburn@gmail.com>
2023-09-04 12:44:00 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sword_and_Sorcery
+{
+    abstract class GameObject
+        {
+        public string name;
+        }
+
+    class Room : GameObject
+    {
+        public Room (string roomName)
+        {
+            name = roomName;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Room throneRoom = new Room("ThroneRoom");
+            Console.WriteLine(throneRoom.name);
+            Console.ReadLine();
+        }
+    }
+}
