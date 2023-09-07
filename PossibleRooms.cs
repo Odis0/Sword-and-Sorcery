91890f54cf79bd491864cbc766141e825ed10758
Spencer Osburn <sjosburn@gmail.com>
2023-09-06 17:32:44 -0700

    I've got the room generator on the back end, I just haven't connected it to the Main. I'm wondering if Rooms should be initialized differently now, because in the possibleRooms list, I've got to put in default x and y values.

---
+++ PossibleRooms.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sword_and_Sorcery
+{
+    public class PossibleRooms
+    {
+        public List<Room> PossibleRoomList { get; }
+
+        public PossibleRooms()
+        {
+            PossibleRoomList = new List<Room>
+            {
+                new Room("Throne Room", "A luxurious throne room.", 0, 0),
+                new Room("Armory", "An array of weapons and armor line the shelves.", 0, 1),
+                new Room("Bobcat Room", "The decor of this room is distinctly bobcat themed, using many of the cats' pelts.", 0, 2),
+                // Add more rooms as needed...
+            };
+
+
+
+        }
+    }
+}
+
---
+++ WorldGenerator.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sword_and_Sorcery
+{
+    public class WorldGenerator
+    {
+        public List<Room> GenerateRooms(int width, int height)
+        {
+            Random random = new Random();
+            PossibleRooms possibleRooms = new PossibleRooms(); // Create an instance of PossibleRooms
+            List<Room> rooms = new List<Room>();
+
+            for (int i = 0; i < width; i++)
+            {
+                for (int j = 0; j < height; j++)
+                {
+                    // Generate random coordinates within the bounds of the game world
+                    int x = random.Next(width);
+                    int y = random.Next(height);
+
+                    // Randomly select a room from the list of possible rooms
+                    Room selectedRoomInfo = possibleRooms.PossibleRoomList[random.Next(possibleRooms.PossibleRoomList.Count)];
+
+                    Room room = new Room(selectedRoomInfo.name, selectedRoomInfo.description, x, y);
+                    rooms.Add(room);
+                }
+            }
+
+            return rooms;
+        }
+    }
+}
+
